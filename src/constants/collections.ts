12317ac85f4af5d87e3309a71d1803396372cdc1
Luna Gondim Cezar <lunagondimcezar@gmail.com>
2026-06-20 18:41:02 -0300

    feat: dynamic recent sections

--- src/constants/collections.ts
+++ src/constants/collections.ts
@@ -1,9 +1,13 @@
 import type { RecentSectionDef } from "../types/recent";
+import { getContentCollections } from "../config/collections";
 
 export const RECENT_PER_COLLECTION = 4;
 
-export const RECENT_SECTIONS: RecentSectionDef[] = [
-  { collection: "art", display: "art" },
-  { collection: "blog", display: "list" },
-  { collection: "wiki", display: "list" },
-];
+const allContent = getContentCollections();
+
+export const RECENT_SECTIONS: RecentSectionDef[] = allContent
+  .toSorted((a) => (a === "art" ? -1 : 1))
+  .map((collection) => ({
+    collection,
+    display: collection === "art" ? ("art" as const) : ("list" as const),
+  }));
